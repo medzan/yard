a254da47ff71a2213c1e5f64c7eed34d25539b7f
Elmehdi zangui <mehdi.zangui@gmail.com>
2018-01-18 16:09:22 +0000

    attached

--- source/yard/src/main/java/com/doc/spring/xml/pojo/XmlEmbeddedPojo.java
+++ source/yard/src/main/java/com/doc/spring/xml/pojo/XmlEmbeddedPojo.java
@@ -1,9 +1,11 @@
 package com.doc.spring.xml.pojo;
 
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import com.doc.spring.instance.Pojo;
-
+@SessionScope 
 public class XmlEmbeddedPojo extends Pojo {
 	
 	private XmlPojo xmlPojo;
