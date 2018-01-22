9507a48d246cdab0a0264e8ca69c331d82f3e96d
Elmehdi zangui <mehdi.zangui@gmail.com>
2018-01-22 15:19:05 +0000

    clean files

--- source/yard/src/main/java/com/doc/spring/javaconfig/Application.java
+++ source/yard/src/main/java/com/doc/spring/javaconfig/Application.java
@@ -5,26 +5,22 @@ package com.doc.spring.javaconfig;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
-
-import com.doc.spring.jc.JcPojo;
 
 /**
  * @author Elmehdi.zangui
  *
  */
 @Configuration
-@ComponentScan(basePackages = "com.doc.spring.jc", includeFilters = @Filter(Component.class))
-@PropertySource("classpath:spring.properties")
+@ComponentScan(basePackages = "com.doc.spring")
 public class Application {
 	 
 	@Bean
--- source/yard/src/main/java/com/doc/spring/jc/JcPojoParam.java
+++
@@ -1,8 +0,0 @@
-package com.doc.spring.jc;
-
-import org.springframework.stereotype.Component;
-
-//@Component
-public class JcPojoParam {
-
-}
