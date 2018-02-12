890cc9c173d11f40fccaa5a6f5a7e80e5f489db8
Elmehdi zangui <mehdi.zangui@gmail.com>
2018-02-12 17:13:04 +0000

    cleaning up

--- source/yard/src/main/java/com/doc/spring/config/Application.java
+++ source/yard/src/main/java/com/doc/spring/config/Application.java
@@ -3,28 +3,8 @@
  */
 package com.doc.spring.config;
 
-
-import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.DependencyDescriptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.format.datetime.DateFormatter;
-import org.springframework.format.datetime.DateFormatterRegistrar;
-import org.springframework.format.datetime.DateTimeFormatAnnotationFormatterFactory;
-import org.springframework.format.datetime.standard.DateTimeFormatterFactoryBean;
-import org.springframework.format.number.NumberFormatAnnotationFormatterFactory;
-import org.springframework.format.support.DefaultFormattingConversionService;
-import org.springframework.format.support.FormattingConversionService;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
  * @author Elmehdi.zangui
